d0d0b21dd1d9c0bcba5efdeea1d3c76b73f28664
Cap2711 <chelsey.penton@keyin.com>
2024-11-18 00:46:36 -0330

    Added all files and added docker

---
+++ GolfClub/src/main/java/com/keyin/GolfClubApiApplication.java
@@ -0,0 +1,12 @@
+package com.keyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfClubApiApplication.class, args);
+    }
+}
